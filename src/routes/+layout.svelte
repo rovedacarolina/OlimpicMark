94002173f1b60a2044b3b7c5d18b1b7d37c99443
Carolina Roveda <rovecarolina@gmail.com>
2026-06-08 17:01:59 +0200

    add custom font (Satoshi)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 
-	import "$lib/syles/reset.css";
+	import "$lib/styles/reset.css";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Satoshi Font -->
+	<link rel="preconnect" href="https://api.fontshare.com/" />
+	<link rel="preconnect" href="https://api.fontshare.com/" crossorigin>
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet">
+
 </svelte:head>
 
 {@render children()}
